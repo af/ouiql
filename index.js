41d78ac2aad06ebba8fd50c5b2b51604e9dcbb91
Aaron Franks <aaron.franks@gmail.com>
2017-07-14 20:39:49 -0700

    Split query context and sanitation into its own module

--- index.js
+++ index.js
@@ -1,47 +1,32 @@
 // @flow
 const assert = require('assert')
 const debug = require('debug')('ouiql')
-
-// Match characters that should be stripped from interpolated static context
-const invalidVarChars = /[^\w, *]/g
+const {sanitize} = require('./lib/queryContext')
 
 
 /*::
 
-import type {Backend, StoreContext} from './lib/backends.js'
+import type {QueryContext} from './lib/queryContext.js'
+import type {Backend} from './lib/backends.js'
 
 type StoreInitOptions = {
     relation: string,
-    context: {[string]: string},
+    context: QueryContext,
     sqlPath: string
 }
 */
-
-
-const sanitizeCtx = (ctx /*:StoreContext*/) /*:StoreContext*/ => {
-    const keys = Object.keys(ctx)
-    return keys.reduce((acc, k) => {
-        const contents /*:string*/ = ctx[k]
-        const rawVar = Array.isArray(contents)
-            ? contents.join(', ')
-            : contents
-        acc[k] = rawVar.replace(invalidVarChars, '')
-        return acc
-    }, {})
-}
 
 exports.makeStore = (backend /*:Backend*/, spec /*:StoreInitOptions*/) => {
     const {relation, context = {}, sqlPath} = spec
     assert(relation, '"relation" must be provided when making a store')
 
     // Set up the static context object that is interpolated into each query
-    const ctx = sanitizeCtx(
-        Object.assign({relation, readFields: '*'}, context)
-    )
+    const defaultCtx = {relation, readFields: '*'}
+    const ctx = sanitize(Object.assign(defaultCtx, context))
 
     const queryFnMap/*:{[string]: function}*/ = backend.buildQueryFunctions({sqlPath, ctx})
 
-    assert(queryFnMap.create, 'create.sql must be present when making a store')
+    assert(queryFnMap.init, 'init.sql must be present when making a store')
     debug(`Store "${relation}" has queries:`, Object.keys(queryFnMap))
 
     return queryFnMap
